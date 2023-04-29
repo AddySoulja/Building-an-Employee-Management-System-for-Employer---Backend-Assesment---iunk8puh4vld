a87d8a5dde204baac8f86dcd983dd46890835799
Aditya Kumar <adityadhanraj1357@gmail.com>
2023-04-29 20:42:23 +0530

    build

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,11 +1,22 @@
-const Employee = require('../models/employeeModel');
+const Employee = require("../models/employeeModel");
 
 //Registering Employee into database
 const createEmployee = async (req, res) => {
   try {
     // Write a code here to store Employee data
+    const employee = await Employee.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      companyName: req.body.companyName,
+      email: req.body.email,
+      salary: req.body.salary,
+    });
+    console.log("new emp: ", employee);
+    res.status(201).json({
+      newEmployee: employee,
+    });
   } catch (err) {
-    res.status(500).json({ error: 'Failed to create employee' });
+    res.status(500).json({ error: "Failed to create employee" });
   }
 };
 
@@ -13,8 +24,11 @@ const createEmployee = async (req, res) => {
 const getEmployee = async (req, res) => {
   try {
     // Write a code here to get Employee from a Particular id
+    const employee = await Employee.find((employee) => employee.id);
+    console.log(employee);
+    res.status(200).json(employee);
   } catch (err) {
-    res.status(500).json({ error: 'Failed to get employee details' });
+    res.status(500).json({ error: "Failed to get employee details" });
   }
 };
 
@@ -23,7 +37,7 @@ const updateEmployee = async (req, res) => {
   try {
     //Write a code here for updating Employee details using 'PUT' request
   } catch (err) {
-    res.status(500).json({ error: 'Failed to update employee details' });
+    res.status(500).json({ error: "Failed to update employee details" });
   }
 };
 
@@ -31,7 +45,7 @@ const deleteEmployee = async (req, res) => {
   try {
     //Write a code here for Deleting all the employees whose salary is greater than 10000
   } catch (err) {
-    res.status(500).json({ error: 'Failed to delete employees' });
+    res.status(500).json({ error: "Failed to delete employees" });
   }
 };
 
