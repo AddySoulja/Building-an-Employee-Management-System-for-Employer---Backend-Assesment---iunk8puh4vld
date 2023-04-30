f2cef83ba7113d29a06026985e99fd9128f256dc
Aditya Kumar <adityadhanraj1357@gmail.com>
2023-04-30 13:24:16 +0530

    Dev

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -24,8 +24,7 @@ const createEmployee = async (req, res) => {
 const getEmployee = async (req, res) => {
   try {
     // Write a code here to get Employee from a Particular id
-    const employee = await Employee.find((employee) => employee.id);
-    console.log(employee);
+    const employee = await Employee.findById(req.params.id);
     res.status(200).json(employee);
   } catch (err) {
     res.status(500).json({ error: "Failed to get employee details" });
@@ -36,6 +35,15 @@ const getEmployee = async (req, res) => {
 const updateEmployee = async (req, res) => {
   try {
     //Write a code here for updating Employee details using 'PUT' request
+    await Employee.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: { ...req.body } }
+    );
+    const employee = await Employee.find();
+    console.log("Updated: ", employee);
+    res.status(201).json({
+      message: "Employee details updated successfully",
+    });
   } catch (err) {
     res.status(500).json({ error: "Failed to update employee details" });
   }
@@ -44,6 +52,22 @@ const updateEmployee = async (req, res) => {
 const deleteEmployee = async (req, res) => {
   try {
     //Write a code here for Deleting all the employees whose salary is greater than 10000
+    const users = await Employee.find();
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].salary > 10000) {
+        await Employee.deleteOne({ _id: users[i].id });
+      }
+    }
+    const newUsers = await Employee.find();
+    if (users.length === newUsers.length) {
+      res.status(404).json({ error: "No employees found" });
+    } else {
+      res.status(200).json({
+        message: "employees deleted successfully",
+        perv: users,
+        next: newUsers,
+      });
+    }
   } catch (err) {
     res.status(500).json({ error: "Failed to delete employees" });
   }
